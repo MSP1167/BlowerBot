47506c4627072dfe63ca2a356c0392dcf50872ab
BTK203 <brach2003@gmail.com>
2019-10-08 20:55:00 -0500

    Generated the project.

---
+++ BlowerBot/src/main/java/frc/robot/OI.java
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * Controllers, button controls, and thats about it.
+ */
+public class OI {
+    public static Joystick DRIVER   = new Joystick(0);
+    public static Joystick OPERATOR = new Joystick(1);
+}
---
+++ BlowerBot/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,68 @@
+//insert cool foximus ASCII art here
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+/**
+ * Robot!
+ */
+public class Robot extends TimedRobot {
+
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    DriverStation.reportWarning("ROBOT INIT, HAVE FUN", false);
+  }
+
+  /**
+   * Called no matter what state the robot is in (disabled, enabled, test, etc)
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * Called when auto starts
+   */
+  @Override
+  public void autonomousInit() {
+    DriverStation.reportWarning("AUTO STARTED!", false);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() {
+    Scheduler.getInstance().run();
+  }
+}
